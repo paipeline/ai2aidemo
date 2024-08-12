a32648091062a52f1c52d079a27c07f63e340442
Pai <ppeng24@wisc.edu>
2024-08-12 18:29:42 +0800

    feat: add message handling in ai2aidemo core module

---
+++ ai2aidemo/core/message.py
@@ -0,0 +1,14 @@
+from agent2 import Agent2
+from abs import abs
+
+
+@
+class Message(abs):
+    """
+    It takes three parameters: agent1 resumes, agent2 resumes, context, topics
+    """
+    def __init__(self, agent1,agent2, context, topics):
+        pass 
+    def
+    
+    
