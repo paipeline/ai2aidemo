b03220b3726fe3e53408dab112d1fa5e2efff501
Pai <ppeng24@wisc.edu>
2024-08-12 18:54:36 +0800

    feat: refactor Message class and implement Question subclass with message handling methods

--- ai2aidemo/core/message.py
+++ ai2aidemo/core/message.py
@@ -1,15 +1,39 @@
 from agent2 import Agent2
-from abs import abs
 from abc import ABC, abstractmethod
 
 
-class Message(abs, ABC):
+class Message(ABC):
     """
     It takes three parameters: agent1 resumes, agent2 resumes, context, topics
     """
-    def __init__(self, agent1,agent2, context, topics):
-        pass 
+    def __init__(self, agent1,agent2):
+        self.agent1 = agent1
+        self.agent2 = agent2
     @abstractmethod
-    def send_message(self):
+    def send_message(self,topic,context):
         pass
+    
+    @abstractmethod 
+    def received_message(self):
+        pass
+    
+class Question(Message):
+    """_summary_
+
+    Args:
+        ABC (_type_): _description_
+    """
+    def __init__(self, agent1, agent2):
+        super().__init__(agent1, agent2, context, topics)
+    
+    def send_message(self,topic:str,context: List):
+        last_message = context[-1]
+    
+        prompt = f""" the agent: {self.agent2.name} here is the last message from {self.agent2.name} and the context: {context[:-1].join("/n")}  Your task now is to give a question about the topic: {topic} that combines your knowledge and his/her experiences"""
+
+    
+
+
+        return self.agent1.inference(prompt)
+    
     
